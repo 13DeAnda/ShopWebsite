313d36f1237c768a644f9af221d66ddf1fa13633
nora de anda <atramulier@noras-MacBook-Pro.local>
2016-07-18 16:56:51 -0700

    product page functional layout

--- front/components/product/product_controller.js
+++ front/components/product/product_controller.js
@@ -1,17 +1,26 @@
 angular.module('black.ProductModule.controller', [])
 .controller('ProductController', ['$scope',
 function($scope){
-
+	//By standar a product must have at least one.
+	
 	$scope.product = {
 		id: 1,
 		title: "some title",
 		price: 56,
 		location: "some location",
 		images: [
-			"https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQcsvljNlUYAOnUWtAYlVaEyeI97Sp8NzhCVMfmbLhLrlYFzF-Z",
+			"https://s-media-cache-ak0.pinimg.com/564x/8c/78/fd/8c78fd82d51f5522f8094c7fd3c8dd3f.jpg",
 			"http://i619.photobucket.com/albums/tt278/_cyanidedoll_/On%20sale/MmMRC1.jpg",
 			"http://i1140.photobucket.com/albums/n562/DarkRomantica/P1060072.jpg"
-		];
+		],
+		description: "this is an example of a description that can be as long as 200 character"
 	};
 
+	$scope.currentImage = $scope.product.images[0]; 
+
+	$scope.imageChange = function(url){
+		$scope.currentImage = url;
+	};
+
+	console.log("the current image", $scope.currentImage);
 }]);
