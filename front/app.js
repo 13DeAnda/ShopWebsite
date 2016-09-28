9e54a4a74d9fbf86f2a92e712e4510bf7c2163d8
nora de anda <atramulier@noras-MacBook-Pro.local>
2016-09-28 16:36:20 -0700

    COOL     adding product thumbnail     improvement on footer and header.

--- front/app.js
+++ front/app.js
@@ -2,7 +2,8 @@ angular.module('black', ['ngRoute',
                         'black.LandingModule.controller',
                         'black.HeaderModule.directives',
                         'black.FooterModule.directives',
-                        'black.ProductModule.controller'
+                        'black.ProductModule.controller',
+                        'black.ProductTumbnailModule.directives'
                         ])
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider
--- front/components/landing/landing_controller.js
+++ front/components/landing/landing_controller.js
@@ -1,4 +1,4 @@
-angular.module('black.LandingModule.controller', ['AjaxModule'])
+angular.module('black.LandingModule.controller', ['AjaxModule', 'black.ProductTumbnailModule.directives'])
 .controller('LandingController', ['$scope', 'ajaxUtil',
 function($scope, ajaxUtil){
 	//TODO: would have to be retrived on api's as either recomendations or most popular.
