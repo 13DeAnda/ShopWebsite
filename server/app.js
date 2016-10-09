52b98481d19aac10d582483a0843c7568a1b7105
nora de anda <atramulier@noras-MacBook-Pro.local>
2016-10-08 21:43:33 -0700

    user login 	better handling of fronted and backend registration and login input 	added location for use on page landing, this shall be moved into a factory

--- server/app.js
+++ server/app.js
@@ -80,25 +80,33 @@ app.get('/api/user/login', function(req, res) {
 		var username = req.query.username;
   	var password = req.query.password;
   	var query = 'select * from Users WHERE username = \''+ username + "\'";
+    var loginData = {};
 
   	client.query(query)
   		.then(function(users){
-  			if(users.rows.length > 0){
+
+  			if(users.rows.length > 0 ){
   				var user = users.rows[0];
+
   				if(bCrypt.compareSync(password, user.password)){
-            var data = {
-              uuid: user.uuid,
-              cart: user.shopcart
-            };
-  					resolve(res.send({data: data}));
+            var newUuid = uuid.v4();
+            loginData.uuid = newUuid,
+            loginData.cart = user.shopcart
+
+            var queryUpdate = "UPDATE Users  SET uuid= \'" + newUuid + "\' WHERE username= \'"+ username +"\'";
+            return client.query(queryUpdate)  					
   				}
   				else{
-  					reject(res.send(400, {error: "invalid password"}));
+  					reject(res.send(401, {error: "invalid password"}));
   				}			
   			}
   			else{
-  				reject(res.send(400, {error: "user not found"}));
-  			} 			
+  				reject(res.send(401, {error: "user not found"}));
+  		  }
+      }.bind(this))
+      .then(function(updated){
+          resolve(res.send({data: loginData}));
+  
   		}.bind(this))
   		.catch(function(err){
   			reject(res.send(err))
@@ -111,18 +119,24 @@ app.post('/api/user/register', function(req, res) {
 
 		var username = req.body.username;
   	var password = req.body.password;
-  	
-  	var query = 'select * from Users WHERE username = \''+ username + "\'";
+  	var regex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
+    //TODO:!!!this shall be passed into a schema testing
+    //TODO:!!! this functions should be tested
+    if(!username || !password){
+      reject(res.send(400, {error: "not username or password sent"}));  
+    }
+    else if(username.length < 6 ||  username.length > 11){
+        reject(res.send(400, {error:"username should be between 6 and 11 characters"}));
+    }
+    else if(!password.match(regex)){
+        reject(res.send(400, {error: "password should contain a minimum of 8 characters at least 1 Alphabet and 1 Number"}));
+    }
+
+    var query = 'select * from Users WHERE username = \''+ username + "\'";
+  	var queryPost = 'INSERT INTO Users VALUES( \''+ username + '\', \'' + encryptedPassword + '\',\'' + newUuid+ '\', Array[0])';
+    var encryptedPassword = bCrypt.hashSync(password, bCrypt.genSaltSync(10), null);  	
     var newUuid = uuid.v4();
 
-    var encryptedPassword = bCrypt.hashSync(password, bCrypt.genSaltSync(10), null);
-
-
-  	var queryPost = 'INSERT INTO Users VALUES( \''+ username + '\', \'' + encryptedPassword + '\',\'' + newUuid+ '\', Array[0])';
-  	
-  	if(!username || !password){
-  		reject(res.send(400, {error: "not username or password sent"}));	
-  	}
   	client.query(query)
   		.then(function(users, err){
   			if(err){
@@ -147,7 +161,5 @@ app.post('/api/user/register', function(req, res) {
   		}.bind(this));
 	}.bind(this));
 });
-
-
 
 var server = app.listen(8000);
