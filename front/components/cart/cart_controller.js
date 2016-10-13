6b4c0cc668cb7e9d4167002dc4038c54f1830bad
nora de anda <atramulier@noras-MacBook-Pro.local>
2016-10-12 19:58:21 -0700

    fix indenttion

--- front/components/cart/cart_controller.js
+++ front/components/cart/cart_controller.js
@@ -2,28 +2,28 @@ angular.module('black.CartModule.controller', ['ngCookies', 'black.HelperUtilMod
 .controller('CartController', ['$scope', '$cookies', 'helperUtility',
 function($scope, $cookies, helperUtility){
 
-	$scope.isMobile = helperUtility.isMobile();
-	console.log("is mobile?", $scope.isMobile);
+  $scope.isMobile = helperUtility.isMobile();
+  console.log("is mobile?", $scope.isMobile);
 
-	$scope.titleValue = $scope.isMobile? 'titleMobile' : 'title';
-	$scope.cartValue = $scope.isMobile? 'cart' : 'cart container';
-	$scope.productValue = $scope.isMobile? 'productMobile' : 'col-lg-6 desc product';
-	//add in the database these products
-	//make service to retrive.
-	//make sure user is logged in
-	//service to go to product when image or title clicked
-	$scope.products = [
-		{
-			did: 3,
-			price: 232,
-			imageSrc: "http://img16.shop-pro.jp/PA01096/409/product/83748679_o1.jpg",
-			title: "sacred crosss dress",
-			quantity: 1,
-		}
-	];
+  $scope.titleValue = $scope.isMobile? 'titleMobile' : 'title';
+  $scope.cartValue = $scope.isMobile? 'cart' : 'cart container';
+  $scope.productValue = $scope.isMobile? 'productMobile' : 'col-lg-6 desc product';
+  //add in the database these products
+  //make service to retrive.
+  //make sure user is logged in
+  //service to go to product when image or title clicked
+  $scope.products = [
+    {
+      did: 3,
+      price: 232,
+      imageSrc: "http://img16.shop-pro.jp/PA01096/409/product/83748679_o1.jpg",
+      title: "sacred crosss dress",
+      quantity: 1,
+    }
+  ];
 
-	$scope.goTo = function(id){
-		console.log("goes to function");
-	};
+  $scope.goTo = function(id){
+    console.log("goes to function");
+  };
 
 }]);
